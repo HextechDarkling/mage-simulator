f8dbd64e56478c63ce4a9d030a8c9a0a480418e6
HextechDarkling <hextechdarkling@gmail.com>
2015-10-06 16:55:25 -0400

    Added empty Unreal 4 project with proper .gitignore

---
+++ unreal/MageSimulator/Source/MageSimulator/MageSimulatorGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MageSimulatorGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGESIMULATOR_API AMageSimulatorGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
